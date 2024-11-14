aa198b63fb2536b59bdfdcbee51d2ae0dde46cec
TejaKumar123 <inceptioncom414@gmail.com>
2024-11-14 13:14:24 +0530

    addproduct completed

--- src/components/AddProduct/AddPreviewCard.jsx
+++ src/components/AddProduct/AddPreviewCard.jsx
@@ -1,19 +1,24 @@
-import { ShoppingCart, Star } from "@mui/icons-material"
+import { Image, ShoppingCart, Star } from "@mui/icons-material"
 
 
-const AddPreviewCard = () => {
+const AddPreviewCard = ({ formik, imageobj }) => {
 	return (
-		<div className="w-[30%] border-[black] border-[0px] flex flex-col items-center justify-start gap-[15px]">
+		<div className="w-[25%] border-[black] border-[0px] flex flex-col items-center justify-start gap-[15px] sticky top-[50px]">
 			<p className="text-[150%] font-bold underline">AddPreviewCard</p>
-			<div className="w-full h-auto bg-white rounded-[6px] p-[10px] flex flex-col items-start justify-start border-[rgba(0,0,0,0.2)] border-[2px] gap-[5px]">
-				<div className="w-[100%] h-[20dvw] border-[black] border-[rgba(0,0,0,0.2)] border-[2px] flex items-center justify-center rounded-[8px] overflow-hidden">
-					<img src="/images/ProductImages/IMG-20241021-WA0030.jpg" className="h-[100%]" />
+			<div className="w-full h-auto bg-white rounded-[6px] p-[10px] flex flex-col items-start justify-start border-[#D9D9D9] border-[2px] gap-[5px]">
+				<div className="w-[100%] h-[15dvw] border-[#D9D9D9] border-[1px] flex items-center justify-center rounded-[8px] overflow-hidden">
+					{imageobj
+						?
+						<img src={URL.createObjectURL(imageobj)} className="max-h-[100%]" />
+						:
+						<Image />}
+
 				</div>
-				<p className="text-[150%] text-[#262626]">Dairy Melts Chocolate bar</p>
-				<p className="text-[#686363]">33 Pcs/Box, 24 Box/Carton</p>
+				<p className="text-[150%] text-[#262626]">{formik.values.name ? formik.values.name : "Name"}</p>
+				<p className="text-[#686363]">{formik.values.Units_Box ? formik.values.Units_Box : "XX"} Pcs/Box, {formik.values.Box_Carton ? formik.values.Box_Carton : "XX"} Box/Carton</p>
 				<div className="w-full h-auto flex flex-row items-center justify-between">
 					<div className="w-auto h-auto flex flex-row items-center justify-between gap-[10px]">
-						<p className="text-[#686363] line-through">&#x20B9;170</p>
+						<p className="text-[#686363] line-through">&#x20B9;{formik.values.Price ? formik.values.Price : "XXX"}</p>
 						<p className="text-[#133DF6] font-bold">3% off</p>
 					</div>
 					<div className="w-auto h-auto flex items-center justify-center gap-[2px] px-[5px] bg-[#133DF6] text-[white] scale-[90%] rounded-md">
@@ -22,8 +27,8 @@ const AddPreviewCard = () => {
 					</div>
 				</div>
 				<div className="w-auto h-auto flex flex-row items-center justify-between gap-[10px]">
-					<p className="text-[160%] font-bold text-[#262626]">&#x20B9;165</p>
-					<p className="font-bold opacity-[90%] scale-[90%]">@&#x20B9;5-per piece</p>
+					<p className="text-[160%] font-bold text-[#262626]">&#x20B9;{formik.values.OfferPrice ? formik.values.OfferPrice : "XX"}</p>
+					<p className="font-bold opacity-[90%] scale-[90%]">@&#x20B9;{formik.values.UnitPrice ? formik.values.UnitPrice : "XX"}-per piece</p>
 				</div>
 
 				<div className=" bg-[#133df6] rounded-md w-auto h-auto flex flex-row items-center justify-between gap-[15px] px-[15px] py-[5px] text-white m-auto">
